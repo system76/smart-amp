f12f9d16c9f2c408e1f91f20faf1859facfe11de
Jeremy Soller <jeremy@system76.com>
2020-06-18 12:49:05 -0600

    Implement register dump

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,37 @@ use std::{
     process,
 };
 
+fn dump_page(amp: &mut Tas5825m, book: u8, page: u8) -> Result<(), String> {
+    println!("book {:02X}", book);
+    amp.set_book(book)?;
+
+    println!("  page {:02X}", page);
+    amp.set_page(page)?;
+
+    for reg in 0x00..=0x7f {
+        println!("    {:02X} {:02X}", reg, amp.read_at(reg)?);
+    }
+
+    Ok(())
+}
+
 fn dump() -> Result<(), String> {
-    let mut dev = I2CDev::new(0, 0x4e)?;
-    println!("{:02X}", dev.read_at(0x67)?);
+    //TODO: check DMI for safety
+    let dev = I2CDev::new(0, 0x4e)?;
+    let mut amp = Tas5825m::new(dev)?;
+
+    dump_page(&mut amp, 0x00, 0x00)?;
+
+    dump_page(&mut amp, 0x78, 0x01)?;
+
+    for page in 0x01..=0x13 {
+        dump_page(&mut amp, 0x8C, page)?;
+    }
+
+    for page in 0x01..=0x10 {
+        dump_page(&mut amp, 0xAA, page)?;
+    }
+
     Ok(())
 }
 
--- src/tas5825m.rs
+++ src/tas5825m.rs
@@ -3,12 +3,43 @@
 
 use crate::i2c::I2CDev;
 
-pub struct Tas5825m {
-    i2c: I2CDev
-}
+pub struct Tas5825m(I2CDev);
 
 impl Tas5825m {
-    pub fn new(i2c: I2CDev) -> Self {
-        Self { i2c }
+    pub fn new(i2c: I2CDev) -> Result<Self, String> {
+        let mut amp = Self(i2c);
+        amp.set_book(0)?;
+        let die_id = amp.read_at(0x67)?;
+        if die_id == 0x95 {
+            Ok(amp)
+        } else {
+            Err(format!("DIE_ID 0x{:02X} does not match 0x95", die_id))
+        }
+    }
+
+    pub fn read_at(&mut self, reg: u8) -> Result<u8, String> {
+        self.0.read_at(reg)
+    }
+
+    pub fn write_at(&mut self, reg: u8, value: u8) -> Result<(), String> {
+        self.0.write_at(reg, value)
+    }
+
+    pub fn get_book(&mut self) -> Result<u8, String> {
+        self.set_page(0x00)?;
+        self.read_at(0x7F)
+    }
+
+    pub fn set_book(&mut self, book: u8) -> Result<(), String> {
+        self.set_page(0x00)?;
+        self.write_at(0x7F, book)
+    }
+
+    pub fn get_page(&mut self) -> Result<u8, String> {
+        self.read_at(0x00)
+    }
+
+    pub fn set_page(&mut self, page: u8) -> Result<(), String> {
+        self.write_at(0x00, page)
     }
 }
